05cb27158815a43612e3ea59edbdaadb7705366c
SHEKHAR SINGH <107771203+SHEKHARSINGH745681@users.noreply.github.com>
2024-10-29 15:09:21 +0530

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,23 @@
+﻿// See https://aka.ms/new-console-template for more information
+using System.Text;
+
+StringBuilder s = new StringBuilder("HELLOSHEKHAR", 20); //limit 20
+
+//s.Append("Shekhar");
+
+//s.AppendLine("\n");//NEXT LINE
+//s.Append("HelloShekharSingh");
+
+// "shekhar" insert after 6th index 
+//s.Insert(6, "shekhar");
+
+
+//index 5 se remove hoga then 3 char. remove hoge
+//s.Remove(5, 3);
+
+// REPLACE MAIN EXIT WORD AND WRITE NEW WORD
+//s.Replace("SHEKHAR", "Geeks For");
+
+s.reverse();
+Console.WriteLine(s);
+
